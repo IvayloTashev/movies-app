8e51261031c0b0dd19e6ab8252273a7f50f4fb94
IvayloTashev <i.tashev@abv.bg>
2024-11-19 18:17:05 +0200

    Add rauting

--- movie-app/src/app/app.component.ts
+++ movie-app/src/app/app.component.ts
@@ -1,30 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MoviesFirebaseService } from './services/movies-firebase.service';
-import { MovieInteface } from './types/movie';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { MainComponent } from "./main/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'movie-app';
-
-  moviesCollection: MovieInteface[] = [];
-
-  constructor(private moviesFirebaseService: MoviesFirebaseService) { }
-
-  ngOnInit(): void {
-    this.moviesFirebaseService.getMovies().subscribe(movies => {
-      this.moviesCollection = movies;
-      console.log(this.moviesCollection);
-    })
-  }
 
 }
--- movie-app/src/app/app.routes.ts
+++ movie-app/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HeroSectionComponent } from './shared/hero-section/hero-section.component';
+import { NotFoundComponent } from './error/not-found/not-found.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { CatalogComponent } from './catalog/catalog/catalog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HeroSectionComponent},
+
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+
+    {path: 'catalog', component: CatalogComponent},
+
+
+    {path: '404', component: NotFoundComponent},
+    {path: '**', redirectTo: '/404'},
+];
