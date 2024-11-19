98e4183dccf7069a980e6102db4756361cdad12b
IvayloTashev <i.tashev@abv.bg>
2024-11-19 18:16:32 +0200

    Add catalog component

---
+++ movie-app/src/app/catalog/catalog/catalog.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieInteface } from '../../types/movie';
+import { MoviesFirebaseService } from '../../services/movies-firebase.service';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-catalog',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './catalog.component.html',
+  styleUrl: './catalog.component.css'
+})
+export class CatalogComponent implements OnInit {
+
+  moviesCollection: MovieInteface[] = [];
+
+  constructor(private moviesFirebaseService: MoviesFirebaseService) { }
+
+  ngOnInit(): void {
+    this.moviesFirebaseService.getMovies().subscribe(movies => {
+      this.moviesCollection = movies;
+      console.log(this.moviesCollection);
+    })
+  }
+}
