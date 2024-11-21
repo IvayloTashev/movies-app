4a6f6472214bd954233e6e90dc350dabb8b0fbc6
IvayloTashev <i.tashev@abv.bg>
2024-11-21 20:19:19 +0200

    Add youtube video functionality

--- movie-app/src/app/details/details.component.ts
+++ movie-app/src/app/details/details.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { MoviesFirebaseService } from '../services/movies-firebase.service';
 import { MovieInteface } from '../types/movie';
 import { ActivatedRoute } from '@angular/router';
+import { YouTubePlayerModule } from '@angular/youtube-player'
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [YouTubePlayerModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent implements OnInit {
   movie = {} as MovieInteface;
+  private apiLoaded = false;
 
   constructor(private moviesFirebaseService: MoviesFirebaseService, private route: ActivatedRoute) { }
 
@@ -22,6 +24,12 @@ export class DetailsComponent implements OnInit {
       this.movie = movieData;
       console.log(this.movie);
     })
-  }
 
+    if (!this.apiLoaded) {
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+      this.apiLoaded = true;
+    }
+  }
 }
