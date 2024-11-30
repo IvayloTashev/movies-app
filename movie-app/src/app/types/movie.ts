71a040c0e7be7aceeaadb24fcd1170c5cf40fa41
IvayloTashev <i.tashev@abv.bg>
2024-11-30 10:23:25 +0200

    Change user and movie interfaces

--- movie-app/src/app/types/movie.ts
+++ movie-app/src/app/types/movie.ts
@@ -9,3 +9,22 @@ export interface MovieInteface {
     id:string,
     trailer: string,
 }
+
+export interface Movie {
+    _ownerId: string,
+    title: string,
+    description: string,
+    img: string,
+    trailer: string,
+    rating: string,
+    genre:  string[],
+    director: string,
+    comments: [],
+    _createdOn: string,
+    _id: string,
+}
+
+
+
+
+
--- movie-app/src/app/types/user.ts
+++ movie-app/src/app/types/user.ts
@@ -1,4 +1,7 @@
 export interface User {
     username: string,
     email: string,
+    password: string,
+    id: string,
+    accessToken: string,
 }
