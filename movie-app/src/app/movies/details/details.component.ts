e8f1d767ff52a134697dde608ec5f62b83996dc1
IvayloTashev <i.tashev@abv.bg>
2024-12-02 18:42:06 +0200

    Add details component

--- movie-app/src/app/movies/details/details.component.ts
+++ movie-app/src/app/movies/details/details.component.ts
@@ -1,27 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../../types/movie';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { YouTubePlayerModule } from '@angular/youtube-player'
 import { ApiService } from '../../services/api.service';
 import { SplitStringPipe } from '../../shared/pipes/split-string.pipe';
+import { UserService } from '../../services/user.service';
+import { User } from '../../types/user';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [YouTubePlayerModule, SplitStringPipe],
+  imports: [YouTubePlayerModule, SplitStringPipe, RouterLink],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent implements OnInit {
   movie = {} as Movie;
+  user = {} as User | null;
+  movieId: string = '';
   private apiLoaded = false;
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService) { }
+  constructor(private activatedRoute: ActivatedRoute, private apiService: ApiService, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
-    const movieId = this.route.snapshot.params['movieId'];
+    const user = this.userService.user$.subscribe((userData) => {
+      this.user = userData;
+    })
+    
+    const id = this.activatedRoute.snapshot.params['movieId'];
+    this.movieId = id;
 
-    this.apiService.getMovieById(movieId).subscribe((movieData) => {
+    this.apiService.getMovieById(this.movieId).subscribe((movieData) => {
       this.movie = movieData;
     })
 
@@ -31,5 +40,11 @@ export class DetailsComponent implements OnInit {
       document.body.appendChild(tag);
       this.apiLoaded = true;
     }
-  }
+  };
+
+  deleteMovie() {
+    this.apiService.deleteMovie(this.movieId).subscribe(() => {
+      this.router.navigate(['/catalog'])
+    })
+  };
 }
