737b8a988c97e5fb98adfc0bb137d5ad65195a6a
IvayloTashev <i.tashev@abv.bg>
2024-12-02 18:41:31 +0200

    Add error message conponent

---
+++ movie-app/src/app/core/error-message/error-message.component.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+import { ErrorMessageService } from './error-message.service';
+
+@Component({
+  selector: 'app-error-message',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-message.component.html',
+  styleUrl: './error-message.component.css'
+})
+export class ErrorMessageComponent {
+  errorMsg = signal('');
+  constructor(private errorMsgService: ErrorMessageService) { }
+
+  ngOnInit(): void {
+    this.errorMsgService.apiError$.subscribe((err: any) => {
+      this.errorMsg.set(err?.message);
+    })
+  }
+}
