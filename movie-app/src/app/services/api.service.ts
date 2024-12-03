4864d8921601b8d8638a71eec81a7181957813cd
IvayloTashev <i.tashev@abv.bg>
2024-12-03 21:29:15 +0200

    Add edit movie service

--- movie-app/src/app/services/api.service.ts
+++ movie-app/src/app/services/api.service.ts
@@ -7,15 +7,15 @@ import { Movie } from '../types/movie';
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getMovies() {
     return this.http.get<Movie[]>(`/api/data/movies`);
-  }
+  };
 
   getMovieById(id: string) {
     return this.http.get<Movie>(`/api/data/movies/${id}`);
-  }
+  };
 
   createMovie(
     title: string,
@@ -23,10 +23,27 @@ export class ApiService {
     img: string,
     trailer: string,
     rating: string,
-    genre:  string,
+    genre: string,
     director: string,
   ) {
-    return this.http.post<Movie>(`/api/data/movies`, {title, description, img, trailer, rating, genre, director});
+    return this.http.post<Movie>(`/api/data/movies`, { title, description, img, trailer, rating, genre, director });
+  };
+
+  editMovie(
+    title: string,
+    description: string,
+    img: string,
+    trailer: string,
+    rating: string,
+    genre: string,
+    director: string,
+    movieId: string
+  ) {
+    return this.http.put<Movie>(`/api/data/movies/${movieId}`, { title, description, img, trailer, rating, genre, director });
+  };
+
+  deleteMovie(movieId: string) {
+    return this.http.delete(`/api/data/movies/${movieId}`);
   }
 
 }
